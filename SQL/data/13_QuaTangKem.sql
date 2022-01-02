fd73eec9c1652edfa9a12f3e5fb9438a8fbea91a
minhtri06 <pmtri.tvv@outlook.com>
2022-01-02 14:36:05 +0700

    Add data: QuaTangKem, DonHang, ChiTietDH, DH_TTinKhac, GiaoHang, BanHang

---
+++ SQL/data/13_QuaTangKem.sql
@@ -0,0 +1,25 @@
+
+-- INSERT QUA TANG
+INSERT INTO QuaTangKem
+    (TenQT, Gia)
+    VALUES
+    (N'Gấu bông', 350000),
+    (N'Thỏ Lily', 450000),
+    (N'Teddy thân thiên', 500000),
+    (N'Husky kèm chăn', 400000),
+    (N'Bánh Kem Tous Les Jours', 380000),
+    (N'Bánh Kem Brodard', 500000),
+    (N'Bánh kem Fresh 1', 500000),
+    (N'Sô-Cô-La D''art', 350000),
+    (N'Giỏ tảo', 500000),
+    (N'Giỏ cam', 350000),
+    (N'Giỏ trái cây nhiều loại', 500000),
+    (N'Nến Thơm', 350000),
+    (N'Hamper thần kỳ', 350000),
+    (N'Bút thần kỳ', 350000),
+    (N'Kẹo ngọt', 380000),
+    (N'Đồng hồ cát', 380000),
+    (N'Sổ tay', 400000),
+    (N'Móc khóa', 4500000),
+    (N'Chuông noel', 380000)    
+GO
